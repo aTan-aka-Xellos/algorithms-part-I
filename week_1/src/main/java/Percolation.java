a8ca673feb586bbe9c8ff89ce5a572996a9e6de3
aTan <atan.aka.xellos@gmail.com>
2018-06-18 01:09:46 +0300

    second submission. clean-up code. remove constructor and fix new bug with bottom row counter

--- week_1/src/main/java/Percolation.java
+++ week_1/src/main/java/Percolation.java
@@ -1,73 +1,75 @@
-import static edu.princeton.cs.algs4.StdRandom.uniform;
-
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * Percolation class.
+ */
 public class Percolation {
 
     private final int dimension;
 
-    private int numberOfOpenSites;
+    private int numberOfOpenSites = 0;
     private boolean[][] states;
 
     private WeightedQuickUnionUF quickUnion;
 
-    // create n-by-n grid, with all sites blocked
+    /**
+     * create n-by-n grid, with all sites blocked.
+     * @param n dimension
+     */
     public Percolation(int n) {
         validateDimension(n);
 
-        dimension  = n;
-
+        dimension = n;
         states = new boolean[n + 1][n + 1];
-
-        for (int i = 1; i < states.length; i++) {
-            for (int j = 1; j < states.length ; j++) {
-                if (uniform(2) == 1) {
-                    states [i][j] = true;
-                    numberOfOpenSites++;
-                }
-            }
-        }
         quickUnion = new WeightedQuickUnionUF(n * n);
-
-//        print(this);
-        
-        // 2 * N^2 - too slow
-        for (int i = 1; i < states.length; i++) {
-            for (int j = 1; j < states.length; j++) {
-                if (isOpen(i, j)) {
-                    unionNeighbors(i, j);
-                }
-            }
-        }
     }
 
-    // open site (row, col) if it is not open already
+    /** Open site (row, col) if it is not open already.
+     *
+     * @param row row
+     * @param col col
+     */
     public void open(int row, int col) {
         validatePrescribedInput(row, col);
 
-        if (isOpen(row, col)) return;
+        if (isOpen(row, col)) {
+            return;
+        }
 
         states [row][col] = true;
         unionNeighbors(row, col);
         numberOfOpenSites++;
     }
 
-    // is site (row, col) open?
+    /**
+     * Is site (row, col) open.
+     * @param row row
+     * @param col col
+     * @return true if open
+     */
     public boolean isOpen(int row, int col) {
         validatePrescribedInput(row, col);
 
         return states[row][col];
     }
 
-    // is site (row, col) full?
+    /**
+     * Is site (row, col) full?
+     * @param row row
+     * @param col col
+     * @return true if full
+     */
     public boolean isFull(int row, int col) {
         validatePrescribedInput(row, col);
 
-        if (!isOpen(row, col)) return false;
+        if (!isOpen(row, col)) {
+            return false;
+        }
 
         for (int topCol = 1; topCol <= dimension; topCol++) {
-            if (!isOpen(1, topCol)) continue;
+            if (!isOpen(1, topCol)) {
+                continue;
+            }
 
             if (quickUnion.connected(xyTo1D(row, col), xyTo1D(1, topCol))) {
                 return true;
@@ -76,8 +78,10 @@ public class Percolation {
         return false;
     }
 
-
-    // does the system percolate?
+    /**
+     * does the system percolate?
+     * @return true if percolates
+     */
     public boolean percolates() {
 
         for (int bottomCol = 1; bottomCol <= dimension; bottomCol++) {
@@ -85,27 +89,21 @@ public class Percolation {
                 continue;
             }
 
-            if (isFull(dimension, bottomCol)) return true;
+            if (isFull(dimension, bottomCol)) {
+                return true;
+            }
         }
         return false;
     }
 
-    // number of open sites
+    /**
+     * number of open sites.
+     * @return number of open sites
+     */
     public int numberOfOpenSites() {
         return numberOfOpenSites;
     }
 
-//    public static void main(String[] args) {
-//
-//        int dimension = 10;
-//
-//        Percolation p = new Percolation(dimension);
-//
-//        StdOut.println("Initiated open sites: " + p.numberOfOpenSites());
-//        StdOut.println("QuickUnion count: " + p.quickUnion.count());
-//        PercolationVisualizer.draw(p, dimension);
-//        StdOut.println("Percolates: " + p.percolates());
-//    }
 
     private void unionNeighbors(int row, int col) {
         int[][] neighbors;
@@ -127,38 +125,49 @@ public class Percolation {
         validatePrescribedInput(row, col);
         int[][] neighbors = new int[4][2];
 
-        neighbors[0] = getLeftNeighbor  (row, col);
-        neighbors[1] = getRightNeighbor (row, col);
+        neighbors[0] = getLeftNeighbor(row, col);
+        neighbors[1] = getRightNeighbor(row, col);
         neighbors[2] = getBottomNeighbor(row, col);
-        neighbors[3] = getTopNeighbor   (row, col);
+        neighbors[3] = getTopNeighbor(row, col);
 
         return neighbors;
     }
 
-    // map from a 2-dimensional (row, column) pair to a 1-dimensional union find object index
     private int xyTo1D(int row, int col) {
         validatePrescribedInput(row, col);
         return (row - 1) * dimension + col - 1;
     }
 
     private int[] getLeftNeighbor(int row, int col) {
-        if (col > 1) return new int[]{row, col - 1};
-        else         return new int[]{};
+        if (col > 1) {
+            return new int[]{row, col - 1};
+        } else {
+            return new int[]{};
+        }
     }
 
     private int[] getRightNeighbor(int row, int col) {
-        if (col < dimension) return new int[]{row, col + 1};
-        else                 return new int[]{};
+        if (col < dimension) {
+            return new int[]{row, col + 1};
+        } else {
+            return new int[]{};
+        }
     }
 
     private int[] getBottomNeighbor(int row, int col) {
-        if (row > 1) return new int[]{row - 1, col};
-        else         return new int[]{};
+        if (row > 1) {
+            return new int[]{row - 1, col};
+        } else {
+            return new int[]{};
+        }
     }
 
     private int[] getTopNeighbor(int row, int col) {
-        if (row < dimension) return new int[]{row + 1, col};
-        else                 return new int[]{};
+        if (row < dimension) {
+            return new int[]{row + 1, col};
+        } else {
+            return new int[]{};
+        }
     }
 
     private static void validatePrescribedInput(int row, int col ) {
@@ -173,17 +182,4 @@ public class Percolation {
         }
     }
 
-    private void print(Percolation p) {
-        for (int i = 1; i < p.states.length; i++) {
-            for (int j = 1; j < p.states.length; j++) {
-                if (p.states[i][j]) {
-                    StdOut.print(1);
-                } else {
-                    StdOut.print(0);
-                }
-                StdOut.print(" ");
-            }
-            StdOut.println();
-        }
-    }
 }
--- week_1/src/main/java/PercolationStats.java
+++ week_1/src/main/java/PercolationStats.java
@@ -1,98 +1,79 @@
 import static edu.princeton.cs.algs4.StdIn.readAllInts;
-import static edu.princeton.cs.algs4.StdOut.println;
 
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
 
     private final double[] threshold;
     private final int trials;
 
-    // perform trials independent experiments on an n-by-n grid
+    /**
+     * perform trials independent experiments on an n-by-n grid
+     * @param dimension dimension
+     * @param trials trials
+     */
     public PercolationStats(int dimension, int trials) {
         validatePercolationStats(dimension, trials);
 
-
         this.trials = trials;
         this.threshold = new double[trials];
-
-        long curr = System.currentTimeMillis();
 
         for (int i = 0; i < trials; i++) {
 
             Percolation percolation = new Percolation(dimension);
 
-            println();
-//            println("openSites start: " + percolation.numberOfOpenSites());
-
-            int counterOpen = 0;
             while(!percolation.percolates()) {
-                int row = StdRandom.uniform(dimension);
-                int col = StdRandom.uniform(dimension);
+                int row = StdRandom.uniform(dimension + 1);
+                int col = StdRandom.uniform(dimension + 1);
 
                 // generated invalid values
                 if (row == 0 || col == 0) continue;
-
                 percolation.open(row, col);
-                counterOpen++;
             }
-
-            println("PERCOLATES!!!");
-            println("takes open: " + counterOpen);
-
 
             threshold[i] = (double)percolation.numberOfOpenSites()
                     / (dimension * dimension);
-
-//            println("openSites end:   " + percolation.numberOfOpenSites());
-            println("trial:           " + i);
-//            println("threshold:       " + threshold[i]);
         }
-
-        StdOut.println(System.currentTimeMillis() - curr + " ms");
-
     }
 
-    // sample mean of percolation threshold
+    /**
+     * sample mean of percolation threshold.
+     * @return mean
+     */
     public double mean() {
         return StdStats.mean(threshold);
     }
 
-    // sample standard deviation of percolation threshold
+    /**
+     * sample standard deviation of percolation threshold.
+     * @return stddev
+     */
     public double stddev() {
         return StdStats.stddev(threshold);
     }
 
-    // low  endpoint of 95% confidence interval
+    /**
+     * low endpoint of 95% confidence interval.
+     * @return confidenceLo
+     */
     public double confidenceLo() {
         return mean() - (1.96 * stddev()/Math.sqrt(trials));
     }
 
-    // high endpoint of 95% confidence interval
+    /**
+     * high endpoint of 95% confidence interval.
+     * @return confidenceHi
+     */
     public double confidenceHi() {
         return mean() + (1.96 * stddev()/Math.sqrt(trials));
     }
 
     public static void main(String[] args) {
 
-        println("Enter dimension and numbers of trials:");
-
         int[] value = readAllInts();
-        println(value[0] + " " + value[1]);
-
         PercolationStats pStats = new PercolationStats(value[0], value[1]);
-
-//        println();
-//        println("mean:      " + pStats.mean());1
-//        println("stddev:    " + pStats.stddev());
-//        println("interval: [" + pStats.confidenceLo()
-//                + ", " + pStats.confidenceHi() + "]");
-        println();
     }
-
 
     private static void validatePercolationStats(int n, int trials ) {
         if (n <= 0 || trials <= 0) {
@@ -100,5 +81,4 @@ public class PercolationStats {
                     + " trials: " + trials);
         }
     }
-
 }
