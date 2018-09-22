229cc1bc01af5eb2308a60a382ef676bd724bf31
aTan <atan.aka.xellos@gmail.com>
2018-09-23 00:00:41 +0300

    Week 3 - Add initial files without implementation

---
+++ week_3/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,17 @@
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ week_3/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,19 @@
+public class FastCollinearPoints {
+
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+
+    // the number of line segments
+    public int numberOfSegments() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+
+    // the line segments
+    public LineSegment[] segments() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
